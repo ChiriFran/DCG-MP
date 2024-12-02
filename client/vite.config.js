5af2ad7887638bd6234b20672ea079b4fe2406cc
Chiri Francisco <franchiri00@gmail.com>
2024-12-02 18:06:48 -0300

    vs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'client/dist' // Configura la salida para que se guarde en client/dist
+    outDir: 'client/dist' 
   }
 });
