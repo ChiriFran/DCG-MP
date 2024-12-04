0b178e744c20204176f3b5fdce306ee833f67955
Chiri Francisco <franchiri00@gmail.com>
2024-12-04 17:50:00 -0300

    vs

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,19 @@
 import "../styles/Footer.css";
+import visaIcon from "../../media/icons/visa.svg";
+import masterIcon from "../../media/icons/mastercard.svg";
+import mpIcon from "../../media/icons//mercadopago.svg";
+
 
 function Footer() {
   return (
     <>
       <section className="footerContainer">
+        <p className="footerPayTitle">Pay with</p>
+        <div className="footerPaymentContainer">
+          <img src={mpIcon} alt="Mercadopago" title="Mercadopago" />
+          <img src={masterIcon} alt="MasterCard" title="MasterCard" />
+          <img src={visaIcon} alt="visa" title="visa" />
+        </div>
         <div className="footer">
           <p className="footerDate">All rights reserved  © 2024 - DCG </p>
         </div>
