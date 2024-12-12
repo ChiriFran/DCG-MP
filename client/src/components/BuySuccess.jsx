158af5e6bd345ea8714f41a3b056e81f06a70645
Chiri Francisco <franchiri00@gmail.com>
2024-12-12 18:35:56 -0300

    vs

--- client/src/components/BuySuccess.jsx
+++ client/src/components/BuySuccess.jsx
@@ -2,24 +2,27 @@ import React, { useEffect } from "react";
 import { doc, updateDoc } from "firebase/firestore";
 import { useOrdenCompraContext } from "../context/OrdenCompraContext";
 import { db } from "../firebase/config";
-import '../styles/Success.css';
+import "../styles/Success.css";
 
 function BuySuccess() {
   const { orderId } = useOrdenCompraContext();
 
   useEffect(() => {
-    if (orderId) {
-      const updateStatus = async () => {
+    const updateStatus = async () => {
+      if (orderId) {
         try {
           const orderRef = doc(db, "pedidos", orderId);
           await updateDoc(orderRef, { status: "success" });
-          console.log(`Estado de la compra actualizado a "success" para el pedido: ${orderId}`);
+          console.log(`Estado actualizado correctamente a "success" para el pedido con ID: ${orderId}`);
         } catch (error) {
           console.error("Error al actualizar el estado del pedido:", error);
         }
-      };
-      updateStatus();
-    }
+      } else {
+        console.warn("El ID del pedido no está definido");
+      }
+    };
+
+    updateStatus();
   }, [orderId]);
 
   return (
