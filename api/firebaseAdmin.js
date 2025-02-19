8dc02a3b8f33fcac4e41f3fb21c9e153b7dab3be
Chiri Francisco <franchiri00@gmail.com>
2025-02-19 15:50:18 -0300

    vs

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,11 +1,21 @@
+// firebaseAdmin.js
+
 import admin from 'firebase-admin';
 
-// Verifica si Firebase ya está inicializado
+// Verificamos si Firebase Admin ya está inicializado
 if (!admin.apps.length) {
+  // Inicialización de Firebase con las credenciales de variables de entorno
   admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Reemplaza saltos de línea
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
   });
+} else {
+  admin.app(); // Si ya está inicializado, utilizamos la instancia existente
 }
 
 const db = admin.firestore();
+
 export { db };
