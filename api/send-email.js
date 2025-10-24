dcba280cf9b140441c51010c9d6414d7d17ab285
Chiri Francisco <franchiri00@gmail.com>
2025-10-24 18:25:09 -0300

    vs

--- api/send-email.js
+++ api/send-email.js
@@ -11,7 +11,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function sendEmail({ to, subject, html }) {
     try {
         const response = await resend.emails.send({
-            from: "no-reply@detroitclassicgallery.com",
+            from: "no-reply@resend.dev",
             to,
             subject,
             html,
